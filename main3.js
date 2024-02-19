cc5e92295905a13d9a96d634643650880aaea47a
Adithyan Ajish <85665413+manuqwert1234@users.noreply.github.com>
2024-02-20 04:07:47 +0530

    Add files via upload
    github

--- main3.js
+++ main3.js
@@ -23,7 +23,7 @@ signInWithEmailAndPassword(auth, email, password)
 .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    window.location.href="../index.html";
+    window.location.href="../home.html";
 })
 .catch((error) => {
     const errorCode = error.code;
